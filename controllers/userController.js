2a0889cdb4cc662bd296c20846a2c38109c4f70a
Muhammed <faraanshaik19@gmail.com>
2025-06-27 20:20:08 +0530

    get users commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,10 +4,10 @@ const jwt = require("jsonwebtoken");
 
 const getUsers = async (req, res) => {
   try {
-    const users = await User.find({});
-    res.status(200).json({ users });
+    const users = await User.find({}, "-password");
+    res.status(200).json(users);
   } catch (err) {
-    res.status(400).json({ msg: "Server error" });
+    res.status(400).json({ msg: "Internal Server error" });
   }
 };
 
@@ -81,4 +81,4 @@ const login = async (req, res) => {
   }
 };
 
-module.exports = { register, login };
+module.exports = { getUsers, register, login };
