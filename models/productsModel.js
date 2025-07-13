4dd19383a833f4a6a34108173dcfaa583850bd8f
Muhammed <faraanshaik19@gmail.com>
2025-07-13 21:13:26 +0530

    products Model commit

--- models/productsModel.js
+++ models/productsModel.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const productsModel = mongoose.Schema({
   title: { type: String, required: true },
+  description: { type: String, required: true },
+  quantity: { type: Number },
 });
 
 module.exports = mongoose.model("product", productsModel);
