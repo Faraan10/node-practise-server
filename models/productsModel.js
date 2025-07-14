47eff42bac87f2c509d744052d2abf5402d31db2
Muhammed <faraanshaik19@gmail.com>
2025-07-14 23:02:23 +0530

    products Schema commit

--- models/productsModel.js
+++ models/productsModel.js
@@ -4,6 +4,7 @@ const productsModel = mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
+  brand: { type: String },
   quantity: { type: Number, default: 1 },
   stock: { type: Number, required: true },
   images: [{ type: String }],
