6c516bfe669ffab24315d17cad72bc3df600bbd5
Muhammed <faraanshaik19@gmail.com>
2025-07-19 23:51:08 +0530

    products controller commit

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,7 +2,7 @@ const Products = require("../models/productsModel");
 
 const getProducts = async () => {
   try {
-    const data = Products.find({});
+    const data = await Products.find({});
     res.status(200).json(data);
   } catch (err) {
     res.status(400).json({ msg: err });
