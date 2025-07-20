895acc1810f1d0e670d3f0367752ffd37d839ec1
Muhammed <faraanshaik19@gmail.com>
2025-07-20 22:07:33 +0530

    fsModule commit

--- fsModule.js
+++ fsModule.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 
 fs.writeFileSync("file2.txt");
 
+fs.readFileSync("file2.txt");
+
 // these below are async operations
 // writing file 1
 // fs.writeFile("file.txt", "Hello file 1", (err) => {
